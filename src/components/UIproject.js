8b9d10f7c6b4445093170eccce87fa70cfffeefb
Abhishek Gupta <ambitiousabhishek7@gmail.com>
2022-06-10 14:51:03 +0530

    abhishek v2

--- src/components/UIproject.js
+++ src/components/UIproject.js
@@ -1,67 +1,78 @@
-import React, { Component } from 'react'
-import './styledButton.css';
-import avatar from './Avatar.jpg'
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
-import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
-import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
-import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
-import CallEndOutlinedIcon from '@mui/icons-material/CallEndOutlined';
+import React, { Component } from "react";
+import "./styledButton.css";
+import avatar from "./Avatar.jpg";
+// import HomeButton from './HomeButton'
 import { GoHome } from "react-icons/go";
-import { BsPerson } from "react-icons/bs";
-import { ImAttachment } from "react-icons/im";
-import { BsPen } from "react-icons/bs";
+import { ImAttachment, ImLinkedin } from "react-icons/im";
+import { BsPen, BsTwitter, BsPerson } from "react-icons/bs";
 import { IoMdPaper } from "react-icons/io";
-
-import { IoMdContact } from "react-icons/io";
-import { BsTwitter } from "react-icons/bs";
+import { VscAccount } from "react-icons/vsc";
 import { RiInstagramLine } from "react-icons/ri";
-import { ImLinkedin } from "react-icons/im";
+// import Elon from "./ElonMusk.jpg";
+import Water from "./Water.jpg";
 
 class UIproject extends Component {
-
-
-
   render() {
     return (
-      
-        <div className='parentProfileDiv' >
-            <div className="profileNameHandleDiv"> 
-
-            
-            
-                <img src={avatar} className="profileImage" />
-                <h1> NAME </h1>
-                <div>
-                <BsTwitter fontSize='large'/><span>   </span>  
-                <RiInstagramLine fontSize='large'/><span>   </span>  
-                <ImLinkedin fontSize='large'/><br/><br/>
-                </div>
+      <div>
+        <div className="parentProfileDiv">
+          <div className="profileNameHandleDiv sidebar-wrapper">
+            <img src={avatar} className="profileImage" alt="Profile" />
+            <h1> NAME </h1>
+            <div className="handleIcons">
+              <BsTwitter className="handleIconsBorder" />
+              <span> </span>
+              <RiInstagramLine className="handleIconsBorder" />
+              <span> </span>
+              <ImLinkedin className="handleIconsBorder" />
+              <br />
+              <br />
             </div>
-          
-            
-            <div className="buttonAlignment">
-                
-                <GoHome/> <button> Home</button><br/><br/>
-                <BsPerson/> <button> About</button><br/><br/>
-                <ImAttachment/>    <button> Resume</button><br/><br/>
-                <BsPen/>    <button> Skills</button><br/><br/>
-                <IoMdPaper/>    <button> Projects</button><br/><br/>
-                <IoMdContact />   <button> Contact</button>
-                
-            </div>
-
           </div>
-          
-        
 
-
-      
-    )
+          <div className="buttonAlignment">
+            <span className="MUI">
+              <GoHome />
+              <button> Home </button>
+              <br />
+              <br />
+            </span>
+            <span className="MUI">
+              <BsPerson />
+              <button> About</button>
+              <br />
+              <br />
+            </span>
+            <span className="MUI">
+              <ImAttachment />
+              <button> Resume</button>
+              <br />
+              <br />
+            </span>
+            <span className="MUI">
+              <BsPen />
+              <button> Skills</button>
+              <br />
+              <br />
+            </span>
+            <span className="MUI">
+              <IoMdPaper />
+              <button> Projects</button>
+              <br />
+              <br />
+            </span>
+            <span className="MUI">
+              <VscAccount />
+              <button> Contact</button>
+            </span>
+          </div>
+        </div>
+        <div>
+          <img src={Water} className="Heroimage" />
+        </div>
+      </div>
+    );
   }
 }
 
-export default UIproject
+export default UIproject;
