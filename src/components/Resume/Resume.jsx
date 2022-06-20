6e945de6c0c74ec4e7f861cdb9a9a61774e70dab
Utkarshking <Utkarshsinghclash@gmail.com>
2022-06-20 10:44:24 +0530

    deleted file resumestylesheet

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BiCloudDownload } from "react-icons/bi";
-import "./ResumeStylesSheet.css";
+import "./ResumeStylesSheet.scss";
 import Table from 'react-bootstrap/Table'
 
 class Resume extends Component {
